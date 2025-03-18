fd2b68832d8dd07c76a2094c38826cc676795d7c
Asmit Patil <asmitpatil1202@gmail.com>
2025-03-18 12:39:23 +0530

    Add files via upload

---
+++ src/components/CardList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from './Card';
+
+const CardList = ({robots}) => {
+  return (
+    <div className='tc'>
+        {robots.map((robot) => (
+        <Card key={robot.id} id={robot.id} name={robot.name} email={robot.email} />
+        ))}
+    </div>
+  )
+}
+
+export default CardList
